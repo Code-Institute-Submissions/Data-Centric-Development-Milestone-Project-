ba1ed4f19077f3364176689bf4aa6b3a366d5961
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-01-15 17:26:36 +0000

    Update Functions
    - Added UPDATE function
    - Added EDIT function

--- app2.py
+++ app2.py
@@ -8,3 +8,28 @@ def submit_review():
               'rating': int(request.form.get('rating'))}
     Reviews.insert_one(review)
     return redirect(url_for('book', isbn=isbn))
+
+# Function to Edit Review
+@app.route("/edit_review", methods=["POST"])
+def edit_review():
+    isbn = request.form.get('isbn')
+    review_id = request.form.get('id')
+    review = Reviews.find_one({'_id': ObjectId(review_id)})
+
+    return render_template("edit.html", review=review, isbn=isbn)
+
+# Function to Update Review
+@app.route("/update_review", methods=["POST"])
+def update_review():
+    review_id = request.form.get('id')
+    old_review = Reviews.find_one({'_id': ObjectId(review_id)})
+
+    isbn = request.form.get('isbn')
+    print(request.form)
+    new_review = {'$set': {'username': request.form.get('username'),
+                           'comments': request.form.get('review'),
+                           'ISBN': isbn,
+                           'rating': request.form.get('rating')}}
+    Reviews.find_one_and_update(
+        old_review, new_review)
+    return redirect(url_for('book', isbn=isbn))
