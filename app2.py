b386d98bcdf15048b67e66bdcf581ae8f9e18410
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-01-20 08:24:43 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,7 @@
 #Review Functions to create, retrieve, update and delete records in MongoDB
 @app.route("/submit_review", methods=["POST"])
 def submit_review():
+    Reviews = mongo.db.reviews
     isbn = request.form.get('isbn')
     review = {'username': request.form.get('username'),
               'comments': request.form.get('review'),
@@ -12,6 +13,7 @@ def submit_review():
 # Function to Edit Review
 @app.route("/edit_review", methods=["POST"])
 def edit_review():
+    Reviews = mongo.db.reviews
     isbn = request.form.get('isbn')
     review_id = request.form.get('id')
     review = Reviews.find_one({'_id': ObjectId(review_id)})
@@ -21,6 +23,7 @@ def edit_review():
 # Function to Update Review
 @app.route("/update_review", methods=["POST"])
 def update_review():
+    Reviews = mongo.db.reviews
     review_id = request.form.get('id')
     old_review = Reviews.find_one({'_id': ObjectId(review_id)})
 
@@ -37,6 +40,7 @@ def update_review():
 # Function to Delete Review
 @app.route("/delete_review", methods=["POST"])
 def delete_review():
+    Reviews = mongo.db.reviews
     review_id = request.form.get('id')
     isbn = request.form.get('isbn')
     Reviews.delete_one({'_id': ObjectId(review_id)})
