0b1a7b2b8a27b19ce826ec7bb3bbbb64d13681cd
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-01-24 22:53:05 +0000

    Submission Commit

--- app.py
+++ app.py
@@ -17,25 +17,21 @@ app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
-#Define method to check if a path exists - used for error handling
+
 def exists(path):
     r = requests.head(path)
     return r.status_code == 200  # pylint: disable=no-member
-#False positive with pylint (known issue)
 
-#Define Search function to GET information from OpenLibrary API
+
 def search_result(search_text):
     resp = requests.get(
         url='http://openlibrary.org/search.json?q=' + search_text)
     result = resp.json()
 
-#Get results from API in JSON format
     search = result['docs']
     result_list = []
-#Sort results in loop to collect 16 items and store them in an object
-#Added static image to handle any book without covers
-    i=0
-    alt_image = "/static/na.png"
+    i = 0
+    alt_image = "/static/images/na.png"
     for item in search:
         if (i == 16):
             break
@@ -48,8 +44,6 @@ def search_result(search_text):
         if len(authors) > 3:
             authors = authors[:4]
         image = 'http://covers.openlibrary.org/b/isbn/{}-M.jpg'.format(isbn)
-#Use ISBN of selected books and pass them as arguements to COVERS API to get an
-#image (if it exists)
         if exists(image):
             image = alt_image
         obj = {
@@ -57,10 +51,10 @@ def search_result(search_text):
             "author": "".join(authors),
             'isbn': isbn,
             'image': image,
-
         }
         result_list.append(obj)
     return result_list
+
 
 @app.route("/")
 def index():
@@ -76,9 +70,8 @@ def results():
         if(len(book_results) > 0):
             flag = 1
         return render_template("results.html", book_results=book_results,
-                               flag=flag)))
+                               flag=flag)
 
-# Display singular book information as selected by User from Results page
 
 @app.route("/book/<isbn>")
 def book(isbn):
@@ -107,12 +100,6 @@ def book(isbn):
     for review in get_reviews:
         review_list.append(review)
 
-# Find any existing reviews in MongoDB
-#    get_reviews = Reviews.find({'ISBN': isbn})
-#    for review in get_reviews:
-#        review_list.append(review)
-
-# Get image for selected book
     image = info.get('cover')
     if image:
         image = image.get('medium')
@@ -123,7 +110,7 @@ def book(isbn):
                            publishers=",".join(publishers), subjects=","
                            .join(subjects))
 
-#Review Functions to create, retrieve, update and delete records in MongoDB
+
 @app.route("/submit_review", methods=["POST"])
 def submit_review():
     Reviews = mongo.db.reviews
@@ -134,6 +121,44 @@ def submit_review():
               'rating': request.form.get('rating')}
     Reviews.insert_one(review)
     return redirect(url_for('book', isbn=isbn))
+
+
+@app.route("/edit_review", methods=["POST"])
+def edit_review():
+    Reviews = mongo.db.reviews
+    isbn = request.form.get('isbn')
+    review_id = request.form.get('id')
+    review = Reviews.find_one_or_404({'_id': ObjectId(review_id)})
+
+    return render_template("edit.html", review=review, isbn=isbn)
+
+
+@app.route("/update_review", methods=["POST"])
+def update_review():
+    Reviews = mongo.db.reviews
+    review_id = request.form.get('id')
+    old_review = Reviews.find_one_or_404({'_id': ObjectId(review_id)})
+
+    isbn = request.form.get('isbn')
+    print(request.form)
+    new_review = {'$set': {'username': request.form.get('username'),
+                           'comments': request.form.get('review'),
+                           'ISBN': isbn,
+                           'rating': request.form.get('rating')}}
+    Reviews.find_one_and_update(
+        old_review, new_review)
+    return redirect(url_for('book', isbn=isbn))
+
+
+@app.route("/delete_review", methods=["POST"])
+def delete_review():
+    Reviews = mongo.db.reviews
+    review_id = request.form.get('id')
+    isbn = request.form.get('isbn')
+    Reviews.delete_one({'_id': ObjectId(review_id)})
+
+    return redirect(url_for('book', isbn=isbn))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
